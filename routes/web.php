55e4d288e133cb7576c912a2599e866c16e73fea
sriwardani140193@gmail.com <qiuling140193>
2018-03-02 20:40:30 +0700

    test routes

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
+
+
+
+Route::get('/', 'SurveyController@home');
+
+Route::get('/survey/new', 'SurveyController@new_survey')->name('new.survey');
+Route::get('/survey/{survey}', 'SurveyController@detail_survey')->name('detail.survey');
+Route::get('/survey/view/{survey}', 'SurveyController@view_survey')->name('view.survey');
+Route::get('/survey/answers/{survey}', 'SurveyController@view_survey_answers')->name('view.survey.answers');
+Route::get('/survey/{survey}/delete', 'SurveyController@delete_survey')->name('delete.survey');
+
+Route::get('/survey/{survey}/edit', 'SurveyController@edit')->name('edit.survey');
+Route::patch('/survey/{survey}/update', 'SurveyController@update')->name('update.survey');
+
+Route::post('/survey/view/{survey}/completed', 'AnswerController@store')->name('complete.survey');
+Route::post('/survey/create', 'SurveyController@create')->name('create.survey');
+
+// Questions related
+Route::post('/survey/{survey}/questions', 'QuestionController@store')->name('store.question');
+
+Route::get('/question/{question}/edit', 'QuestionController@edit')->name('edit.question');
+Route::patch('/question/{question}/update', 'QuestionController@update')->name('update.question');
+Route::auth();
+
+
+// Route::resource('/survey', 'survey/view.blade.php');
